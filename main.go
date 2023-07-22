35ee1cacf4761b6a502ccd255e95924e6c02f0c1
Adriano Wichmann <adriano@agidesk.com.br>
2023-07-22 15:26:07 -0300

    feat: add opening schema, custom logger

--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/adwichmann/gostart.git/router"
+import (
+	"github.com/adwichmann/gostart.git/config"
+	"github.com/adwichmann/gostart.git/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	//initialize configs
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+
 	router.Initialize()
 }
